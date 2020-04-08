5bfdd8db905e9916d2a7b1b68250852e5ac7921c
gkennedy982 <gkennedy982@gmail.com>
2020-04-08 01:56:48 +0100

    Database creation 1

---
+++ DataCreation/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dnd_character_sheet;
+
+namespace DataCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DndCharacterData db = new DndCharacterData();
+            using(db)
+            {
+                #region Races
+                Race R1 = new Race()
+                {
+                    RaceId = 1,
+                    Name = "Humans",
+                    AbilityScore = 1,
+                    Alignment = "Neutral",
+                    Size = "5'5-6'0(medium)",
+                    Speed = 30,
+                    Languages = "English",
+
+                };
+                Race R2 = new Race()
+                {
+                    RaceId = 2,
+                    Name = "Dwarves",
+                    AbilityScore = 2,
+                    Alignment = "Lawful Good",
+                    Size = "5'0 or lower(small)",
+                    Speed = 25,
+                    Languages = "Dwarvish"
+                };
+                Race R3 = new Race()
+                {
+                    RaceId = 3,
+                    Name = "Elves",
+                    AbilityScore = 2,
+                    Alignment = "Chaotic Evil",
+                    Size = "6'1+(Large)",
+                    Speed = 35,
+                    Languages = "Elvish",
+                };
+                #endregion
+                #region Classes
+                Classes C1 = new Classes()
+                {
+                    Name = "Fighter",
+                    ClassID = 1,
+                    HitDice = 10,
+                    FirstLevelHP = 10,
+                    SavingThrow1 = "Strength",
+                    SavingThrow2 = "Constitution",
+                    Skill1 = "Acrobatics",
+                    Skill2 = "Animal Handeling",
+                    Skill3 = "Athletics",
+                    Skill4 = "History",
+                    Skill5 = "Perception",
+                    Skill6 = "Intimidation",
+                };
+                Classes C2 = new Classes()
+                {
+                    Name = "Bard",
+                    ClassID = 2,
+                    HitDice = 8,
+                    FirstLevelHP = 8 + 1,
+                    SavingThrow1 = "Dexterity",
+                    SavingThrow2 = "Charisma",
+                    Skill1 = "Any",
+                    Skill2 = "Any",
+                    Skill3 = "Any",
+                    Skill4 = "n/a",
+                    Skill5 = "n/a",
+                    Skill6 = "n/a",
+                    
+
+                };
+                Classes C3 = new Classes()
+                {
+                    Name = "Rogue",
+                    ClassID = 3,
+                    HitDice = 8,
+                    FirstLevelHP = 8 + 2,
+                    SavingThrow1 = "Dexterity",
+                    SavingThrow2 = "Intelligence",
+                    Skill1 = "Stealth",
+                    Skill2 = "Sleight Of Hand",
+                    Skill3 = "Acrobatics",
+                    Skill4 = "Athletics",
+                    Skill5 = "Deception",
+                    Skill6 = "Perception",
+                };
+                #endregion
+
+            }
+
+
+        }
+    }
+}
---
+++ Dnd character sheet/Race.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dnd_character_sheet
+{
+    public class Race
+    {
+        public int RaceId { get; set; }
+        public string Name { get; set; }
+        public int AbilityScore { get; set; }
+        public string Alignment { get; set; }
+        public string Size { get; set; }
+        public int Speed { get; set; }
+        public string Languages { get; set; }
+        
+
+    }
+    public class Classes
+    {
+        public string Name { get; set; }
+        public int ClassID { get; set; }
+        public int HitDice { get; set; }
+        public int FirstLevelHP { get; set; }
+        public string SavingThrow1 { get; set; }
+        public string SavingThrow2 { get; set; }
+        public string Skill1 { get; set; }
+        public string Skill2 { get; set; }
+        public string Skill3 { get; set; }
+        public string Skill4 { get; set; }
+        public string Skill5 { get; set; }
+        public string Skill6 { get; set; }
+        
+        public virtual List<Feat> feats { get; set; }
+        
+    }
+    public class Feat
+    {
+        public int FeatID { get; set; }
+        public int ClassID { get; set; }
+        public string Description { get; set; }
+    }
+    public class DndCharacterData : DbContext
+    {
+        public DndCharacterData() : base("DnDdata") { }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Classes> Classes { get; set; }
+        public DbSet<Feat> Feats { get; set; }
+    }
+}
